fb15c9e9dcebec55847107d2a584bc9d8122f185
Sameera madusanka <sameera234madusanka@gmail.com>
2021-11-04 15:53:12 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgxEditorModel } from 'ngx-monaco-editor';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,116 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'new-code-editor';
+  monacod = (<any>window).monaco;
+  suggestionSet:any[]=[];
+  
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe(data => {
+        this.suggestionSet = data;
+    });
+    
+  }
+
+  options = {
+    theme: 'myCoolTheme',
+    fontFamily: "Courier",
+    fontSize: 12
+  };
+
+  model: NgxEditorModel = {
+    value: "",
+    language: 'mySpecialLanguage'
+  };
+
+  public getJSON(): Observable<any> {
+    return this.http.get("../assets/wordset.json");
+  }
+
+  onInit(editor:any) {
+
+    const monaco = (<any>window).monaco;
+    const CompletionItemKindList=[
+      {"label":"Class", "value":(<any>window).monaco.languages.CompletionItemKind.Class},
+      {"label":"Color", "value":(<any>window).monaco.languages.CompletionItemKind.Color},
+      {"label":"Constant", "value":(<any>window).monaco.languages.CompletionItemKind.Constant},
+      {"label":"Constructor", "value":(<any>window).monaco.languages.CompletionItemKind.Constructor},
+      {"label":"Customcolor", "value":(<any>window).monaco.languages.CompletionItemKind.Customcolor},
+      {"label":"Enum", "value":(<any>window).monaco.languages.CompletionItemKind.Enum},
+      {"label":"EnumMember", "value":(<any>window).monaco.languages.CompletionItemKind.EnumMember},
+      {"label":"Event", "value":(<any>window).monaco.languages.CompletionItemKind.Event},
+      {"label":"Field", "value":(<any>window).monaco.languages.CompletionItemKind.Field},
+      {"label":"File", "value":(<any>window).monaco.languages.CompletionItemKind.File},
+      {"label":"Folder", "value":(<any>window).monaco.languages.CompletionItemKind.Folder},
+      {"label":"Function", "value":(<any>window).monaco.languages.CompletionItemKind.Function},
+      {"label":"Interface", "value":(<any>window).monaco.languages.CompletionItemKind.Interface},
+      {"label":"Issue", "value":(<any>window).monaco.languages.CompletionItemKind.Issue},
+      {"label":"Keyword", "value":(<any>window).monaco.languages.CompletionItemKind.Keyword},
+      {"label":"Method", "value":(<any>window).monaco.languages.CompletionItemKind.Method},
+      {"label":"Module", "value":(<any>window).monaco.languages.CompletionItemKind.Module},
+      {"label":"Operator", "value":(<any>window).monaco.languages.CompletionItemKind.Operator},
+      {"label":"Property", "value":(<any>window).monaco.languages.CompletionItemKind.Property},
+      {"label":"Reference", "value":(<any>window).monaco.languages.CompletionItemKind.Reference},
+      {"label":"Snippet", "value":(<any>window).monaco.languages.CompletionItemKind.Snippet},
+      {"label":"Struct", "value":(<any>window).monaco.languages.CompletionItemKind.Struct},
+      {"label":"Text", "value":(<any>window).monaco.languages.CompletionItemKind.Text},
+      {"label":"Unit", "value":(<any>window).monaco.languages.CompletionItemKind.Unit},
+      {"label":"User", "value":(<any>window).monaco.languages.CompletionItemKind.User},
+      {"label":"value", "value":(<any>window).monaco.languages.CompletionItemKind.value},
+      {"label":"Variable", "value":(<any>window).monaco.languages.CompletionItemKind.Variable}
+    ];
+    
+    this.suggestionSet.forEach((ele,ind)=>{
+      CompletionItemKindList.forEach(ele1=>{
+        if(ele.itemkind == ele1.label){
+          this.suggestionSet[ind].kind = ele1.value;
+        }
+      });
+    });
+    
+    const wordSet = this.suggestionSet;
+    
+    monaco.languages.registerHoverProvider('mySpecialLanguage', {
+      provideHover: function (model:any, position:any) {
+        if(model.getWordAtPosition(position)!= null){
+          var result = wordSet.find(obj => {
+            return obj.insertText === model.getWordAtPosition(position).word || obj.label === model.getWordAtPosition(position).word;
+          })
+          return {
+            
+            contents: [
+              { value: model.getWordAtPosition(position).word },
+              { value: result?result.documentation:"" }
+            ]
+          }
+        }
+        else{
+          return;
+        }
+          
+      }
+    });
+
+
+    monaco.languages.registerCompletionItemProvider('mySpecialLanguage', {
+      provideCompletionItems: function(model:any, position:any) {
+          var word = model.getWordUntilPosition(position);
+          var range = {
+              startLineNumber: position.lineNumber,
+              endLineNumber: position.lineNumber,
+              startColumn: word.startColumn,
+              endColumn: word.endColumn
+          };
+
+          wordSet.forEach(word=>{
+            word.range = range;
+          });
+          return {
+              
+              suggestions: wordSet
+          };
+      }
+  });
+
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,163 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MonacoEditorModule, NgxMonacoEditorConfig  } from 'ngx-monaco-editor';
 import { AppComponent } from './app.component';
+
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  baseUrl: 'assets', // configure base path cotaining monaco-editor directory after build default: './assets'
+  defaultOptions: { scrollBeyondLastLine: true }, // pass default options to be used
+  onMonacoLoad: () => {
+    // here monaco object will be available as window.monaco use this function to extend monaco editor functionalities.
+    (<any>window).monaco.languages.register({ id: 'mySpecialLanguage' });
+
+    (<any>window).monaco.languages.setMonarchTokensProvider('mySpecialLanguage', {
+
+      keywords: [
+        'map', 'flatMap', 'filter', 'sideEffect', 'branch'
+      ],
+
+      typeKeywords: [
+        'boolean', 'double', 'byte', 'int', 'short', 'char', 'void', 'long', 'float'
+      ],
+
+      operators: [
+        '=', '>', '<', '!', '~', '?', ':', '==', '<=', '>=', '!=',
+        '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^', '%',
+        '<<', '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=', '^=',
+        '%=', '<<=', '>>=', '>>>='
+      ],
+    
+      // we include these common regular expressions
+      symbols:  /[=><!~?:&|+\-*\/\^%]+/,
+    
+      // C# style strings
+      escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+    
+
+      tokenizer: {
+        root: [
+          [/[a-z_$][\w$]*/, { 
+            cases: { 
+              '@typeKeywords': 'keyword',
+              '@keywords': 'keyword',
+              '@default': 'identifier' 
+            } 
+          }],
+                                   
+          [/[A-Z][\w\$]*/, 'type.identifier' ],  // to show class names nicely
+
+          // whitespace
+          { include: '@whitespace' },
+
+          // delimiters and operators
+          [/[{}()\[\]]/, '@brackets'],
+          [/[<>](?!@symbols)/, '@brackets'],
+          [/@symbols/, { cases: { '@operators': 'operator',
+                                  '@default'  : '' } } ],
+
+          // @ annotations.
+          // As an example, we emit a debugging log message on these tokens.
+          // Note: message are supressed during the first load -- change some lines to see them.
+          [/@\s*[a-zA-Z_\$][\w\$]*/, { token: 'annotation', log: 'annotation token: $0' }],
+
+          // numbers
+          [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+          [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+          [/\d+/, 'number'],
+
+          // delimiter: after number because of .\d floats
+          [/[;,.]/, 'delimiter'],
+
+          // strings
+          [/"([^"\\]|\\.)*$/, 'string.invalid' ],  // non-teminated string
+          [/"/,  { token: 'string.quote', bracket: '@open', next: '@string' } ],
+
+          // characters
+          [/'[^\\']'/, 'string'],
+          [/(')(@escapes)(')/, ['string','string.escape','string']],
+          [/'/, 'string.invalid']
+        ],
+        comment: [
+          [/[^\/*]+/, 'comment' ],
+          [/\/\*/,    'comment', '@push' ],    // nested comment
+          ["\\*/",    'comment', '@pop'  ],
+          [/[\/*]/,   'comment' ]
+        ],
+    
+        string: [
+          [/[^\\"]+/,  'string'],
+          [/@escapes/, 'string.escape'],
+          [/\\./,      'string.escape.invalid'],
+          [/"/,        { token: 'string.quote', bracket: '@close', next: '@pop' } ]
+        ],
+    
+        whitespace: [
+          [/[ \t\r\n]+/, 'white'],
+          [/\/\*/,       'comment', '@comment' ],
+          [/\/\/.*$/,    'comment'],
+        ],
+      }
+    });
+
+    (<any>window).monaco.editor.defineTheme('myCoolTheme', {
+      base: 'vs',
+      inherit: false,
+      rules: [
+        // { token: 'keyword', foreground: '8082d9', fontStyle: 'bold' },
+        // { token: 'identifier', foreground: '3681eb' },
+        // { token: 'comment', foreground: '00FF00' },
+        // { token: 'operator', foreground: '00FF00' },
+        // { token: '', foreground: 'efff12' }
+
+        { token: 'comment', foreground: 'aaaaaa', fontStyle: 'italic' },
+        { token: 'keyword', foreground: 'ce63eb' },
+        { token: 'operator', foreground: '000000' },
+        { token: 'namespace', foreground: '66afce' },
+
+        { token: 'type', foreground: '1db010' },
+        { token: 'struct', foreground: '0000ff' },
+        { token: 'class', foreground: '0000ff', fontStyle: 'bold' },
+        { token: 'interface', foreground: '007700', fontStyle: 'bold' },
+        { token: 'enum', foreground: '0077ff', fontStyle: 'bold' },
+        { token: 'typeParameter', foreground: '1db010' },
+        { token: 'function', foreground: '94763a' },
+
+        { token: 'member', foreground: '94763a' },
+        { token: 'macro', foreground: '615a60' },
+        { token: 'variable', foreground: '3e5bbf' },
+        { token: 'parameter', foreground: '3e5bbf' },
+        { token: 'property', foreground: '3e5bbf' },
+        { token: 'label', foreground: '615a60' },
+
+        { token: 'type.static', fontStyle: 'bold' },
+        { token: 'class.static', foreground: 'ff0000', fontStyle: 'bold' }
+      ],
+      colors: {
+        'editor.foreground': '#000000',
+        'editor.background': '#87CEEB',
+        'editorCursor.foreground': '#8B0000',
+        'editor.lineHighlightBackground': '#0000FF20',
+        'editorLineNumber.foreground': '#008800',
+        'editor.selectionBackground': '#88000030',
+        'editor.inactiveSelectionBackground': '#88000015'
+    }
+    });
+  }
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MonacoEditorModule.forRoot(monacoConfig) // use forRoot() in main app module only.
   ],
   providers: [],
   bootstrap: [AppComponent]
